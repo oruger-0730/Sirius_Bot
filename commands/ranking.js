1f1dc2028255ac6d99d44edd68faf78bc9a6255c
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-02-24 22:45:46 +0900

    Update ranking command to use saved username from economy.json

--- commands/ranking.js
+++ commands/ranking.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
-const economyData = JSON.parse(fs.readFileSync('economy.json'));
 
-function getUserName(userId) {
-    const userEconomy = economyData[userId];
-    if (userEconomy && userEconomy.username) {
-        return userEconomy.username;
-    }
-    return user.tag;
-}
+const ranking = async (message) => {
+    const economyData = JSON.parse(fs.readFileSync('economy.json'));
+    const rankings = Object.keys(economyData).map(userId => ({
+        userId,
+        username: economyData[userId].username || '<Unknown>',
+        balance: economyData[userId].balance
+    }));
 
-// Other ranking logic...
+    rankings.sort((a, b) => b.balance - a.balance);
+    
+    let response = 'Ranking:\n';
+    rankings.forEach((user, index) => {
+        response += `${index + 1}. ${user.username}: ${user.balance}\n`;
+    });
+
+    message.channel.send(response.trim());
+};
+
+module.exports = { ranking };
