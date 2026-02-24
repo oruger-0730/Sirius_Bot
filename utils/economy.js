6568fa3901b6f80db862bc138ee3cbbdaad7e127
オルガー <135433478+oruger-0730@users.noreply.github.com>
2026-02-24 22:43:18 +0900

    Add username field to economy.json storage

--- utils/economy.js
+++ utils/economy.js
@@ -1,15 +1,14 @@
-function setUserEconomy(userId, balance, username) {
-    // Logic to set user economy
-    return { userId, balance, username };
-}
-
-function getUserEconomy(userId) {
-    // Logic to get user economy
-    const economyData = {}; // Fetch economy data here
-    return { ...economyData, username: economyData.username };
-}
-
-function getRanking() {
-    const rankingData = []; // Fetch ranking data here
-    return rankingData.map(entry => ({ ...entry, username: entry.username }));
+{
+  "economyData": [
+    {
+      "userId": "12345",
+      "balance": 100,
+      "username": "User1"
+    },
+    {
+      "userId": "67890",
+      "balance": 200,
+      "username": "User2"
+    }
+  ]
 }
